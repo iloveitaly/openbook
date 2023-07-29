fb2ecdc5311d56d61554d413bde99434eddb3c80
Michael Bianco <iloveitaly@gmail.com>
2023-07-29 11:39:48 -0600

    fix: clear crawlee cache completely after each run
    I'm not sure exactly why it doesn't work by default

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,53 @@
 // For more information, see https://crawlee.dev/
-import { Dataset, PlaywrightCrawler } from "crawlee"
+import {
+  Configuration,
+  Dataset,
+  LogLevel,
+  PlaywrightCrawler,
+  purgeDefaultStorages,
+} from "crawlee"
 
-import { router } from "./routes.js"
+import { createRouter } from "./routes.js"
 
 export default async function crawl(initialUrl: string) {
   console.log(`crawling ${initialUrl}`)
 
   const startUrls = [initialUrl]
 
-  const crawler = new PlaywrightCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
-    requestHandler: router,
+  // const config = new Configuration({ persistStorage: false })
+  const config = new Configuration({
+    logLevel: LogLevel.DEBUG,
+    persistStorage: false,
+    storageClientOptions: {
+      persistStorage: false,
+    },
   })
+
+  Configuration.getGlobalConfig().set("persistStorage", false)
+  Configuration.getGlobalConfig().set("storageClientOptions", {
+    ...Configuration.getGlobalConfig().get("storageClientOptions"),
+    persistStorage: false,
+  })
+
+  const crawler = new PlaywrightCrawler(
+    {
+      // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+      requestHandler: createRouter(),
+    },
+    config
+  )
 
   await crawler.addRequests(startUrls)
   await crawler.run()
 
   const data = await Dataset.getData()
+  const urls = data.items
+
+  ;(await Dataset.open()).drop()
+  await purgeDefaultStorages()
+
+  // await crawler.requestQueue.drop()
+  await crawler.teardown()
+
   return data.items
 }
--- src/routes.ts
+++ src/routes.ts
@@ -1,23 +1,29 @@
 import { Dataset, createPlaywrightRouter } from "crawlee"
 
-export const router = createPlaywrightRouter()
+export function createRouter() {
+  const router = createPlaywrightRouter()
 
-router.addDefaultHandler(async ({ enqueueLinks, log }) => {
-  log.info(`enqueueing new URLs`)
+  router.addDefaultHandler(async ({ enqueueLinks, log }) => {
+    log.info(`enqueueing new URLs`)
 
-  await enqueueLinks({
-    // globs: ['https://crawlee.dev/**'],
-    // limit: 1000,
-    label: "detail",
+    await enqueueLinks({
+      // globs: ['https://crawlee.dev/**'],
+      // limit: 1000,
+      label: "detail",
+    })
   })
-})
 
-router.addHandler("detail", async ({ request, page, log }) => {
-  const title = await page.title()
-  log.info(`${title}`, { url: request.loadedUrl })
+  router.addHandler("detail", async ({ request, page, log }) => {
+    const title = await page.title()
+    // TODO maybe we should pull the description from the page as well?
 
-  await Dataset.pushData({
-    url: request.loadedUrl,
-    title,
+    log.info(`${title}`, { url: request.loadedUrl })
+
+    await Dataset.pushData({
+      url: request.loadedUrl,
+      title,
+    })
   })
-})
+
+  return router
+}
